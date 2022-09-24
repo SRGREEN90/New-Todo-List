d9dcae1cbf8849a080925f6cbd23068a7928a00b
admin <1111>
2022-09-24 20:31:00 +0300

    'added checked function'

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react'
+import React, {ChangeEvent, FC, useState} from 'react'
 import {FilterValuesType, TaskType} from "../App";
 import s from "./TodoList.module.css";
 
@@ -8,35 +8,53 @@ type TodolistPropsType = {
     title: string
     taskFilter: (filterValue: FilterValuesType) => void
     removeTasks: (id: number)=> void
+    addTask: (newTitle: string) => void
+    changeTaskStatus: (taskId: number, isDone: boolean) => void
 }
 
 const Todolist: FC<TodolistPropsType> = ({
-                                             tasks,
-                                             title,
-                                             taskFilter,
-                                             removeTasks
+               tasks,
+               title,
+               taskFilter,
+               removeTasks,
+               addTask,
+               changeTaskStatus
 } ) => {
+   let [taskTitle, setTaskTitle] = useState<string>('')
+
+   let onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+       setTaskTitle(e.target.value)
+   }
+    let addTaskTitle = () => addTask(taskTitle)
+
 
     let TasksForRender = tasks.map(t => {
+        let onCheckedHandler = (e: ChangeEvent<HTMLInputElement>) => {
+            changeTaskStatus(t.id, e.currentTarget.checked)
+        }
+        let removeTaskHandler = () => removeTasks(t.id)
         return(
             <li key={t.id}>
-                <input type='checkbox' checked={t.isDone}/>
+                <input
+                    type='checkbox'
+                    checked={t.isDone}
+                    onChange={onCheckedHandler}
+                />
                 <span>{t.title}</span>
-                <button onClick={() => removeTasks(t.id)}>X</button>
+                <button onClick={removeTaskHandler}>X</button>
             </li>
 
         )
 
     })
 
-
-    return <div >
+    return <div>
         <h3 style={{color: 'red'}}>{title}</h3>
         <div>
-            <input/>
-            <button>+</button>
+            <input onChange={onChangeTitle}/>
+            <button onClick={addTaskTitle}>+</button>
         </div>
-           <div >
+           <div>
                {TasksForRender}
            </div>
         <div>
