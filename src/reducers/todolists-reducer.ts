9907ee067338b3dbb1a298e4dbf1011c68516ae7
admin <1111>
2022-10-25 23:31:22 +0300

    'added new Cases'

--- src/reducers/todolists-reducer.ts
+++ src/reducers/todolists-reducer.ts
@@ -1,4 +1,4 @@
-import {TodolistType} from "../App";
+import {FilterValuesType, TodolistType} from "../App";
 import {v1} from "uuid";
 
 type RemoveTodoList = {
@@ -10,12 +10,25 @@ type AddTodoList = {
     type: "ADD-TODOLIST"
     title: string
 }
+type changeTodoListTitle = {
+    type: "CHANGE-TODOLIST-TITLE"
+    todolistId: string
+    newTitle: string
+}
+
+type changeTodoListFilter = {
+    type: "CHANGE-TODOLIST-FILTER"
+    todolistId: string
+    newFilter: FilterValuesType
+}
+
+
+type ActionType = changeTodoListFilter | changeTodoListTitle | AddTodoList | RemoveTodoList
 
 
 export const todoListsReducer = (
                 todoLists: TodolistType[],
-                action: RemoveTodoList | AddTodoList
-                                ): TodolistType[] => {
+                action: ActionType): TodolistType[] => {
 
     switch (action.type) {
         case "REMOVE-TODOLIST":
@@ -25,6 +38,11 @@ export const todoListsReducer = (
             const newTodoList: TodolistType = {id: newTodoID, title: action.title, filter: 'all'}
             return [...todoLists, newTodoList]
                    // {...tasks, [newTodoID]: [] }
+        case "CHANGE-TODOLIST-TITLE":
+         return todoLists.map((tl) => tl.id === action.todolistId ? {...tl, title: action.newTitle }: tl)
+
+        case "CHANGE-TODOLIST-FILTER":
+          return todoLists.map((tl) => tl.id === action.todolistId ? {...tl, filter: action. newFilter}: tl)
         default:
             return todoLists
 
